d274a21ffce77321af5fc537050b4d776dcb7aab
guan xin <guanxin@tinytiger.cn>
2020-12-09 18:29:29 +0800

    submit

--- app/Controllers/MenuController.php
+++ app/Controllers/MenuController.php
@@ -9,11 +9,21 @@ class MenuController extends Controller
 {
     public function indexAction()
     {
+
         $keyword = input('keyword', ['string', 'default' => '']);
+        if (is_null($keyword)) {
+            if ($this->redisDb->get('doniai:navs')) {
+                return json_parse($this->redisDb->get('doniai:navs'));
+            }
+        }
         $query = NavMenu::select(['menu_id', 'name', 'logo_type', 'icon_suffix', 'image_url', 'jump_url', 'color'])->where(['enabled' => 1]);
         if (!is_null($keyword)) {
             $query->whereContains('name', $keyword);
+            return $query->all();
+        } else {
+            $data = $query->all();
+            $this->redisDb->set('doniai:navs', json_stringify($data));
+            return $data;
         }
-        return $query->all();
     }
 }
